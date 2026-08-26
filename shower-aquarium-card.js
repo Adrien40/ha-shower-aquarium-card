ba22cb77b88277a7832e42ed0502c155967b9a99
Adrien <101044689+Adrien40@users.noreply.github.com>
2026-08-26 08:21:41 +0200

    Update shower-aquarium-card.js

--- shower-aquarium-card.js
+++ shower-aquarium-card.js
@@ -1,9 +1,4 @@
-import {
-  LitElement,
-  html,
-  css,
-  svg,
-} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
+import { LitElement, html, css, svg } from "./lit-element-bundle.min.js";
 
 const TRANSLATIONS = {
   en: {
